05e4b696671f481b03536d2b93d74b1f12bc7d35
Galih Daniel Wicaksono <wicaksonogalih21@gmail.com>
2025-04-04 01:57:21 +0900

    commit tugas4

--- app/src/main/java/com/example/tugas2/MainActivity.kt
+++ app/src/main/java/com/example/tugas2/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.tugas2
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
