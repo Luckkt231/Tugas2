38625b728676f730c760201b648b88ff533f1d2b
Galih Daniel Wicaksono <wicaksonogalih21@gmail.com>
2025-04-04 01:35:06 +0900

    commit tugas4

--- app/src/main/java/com/example/tugas2/MainActivity.kt
+++ app/src/main/java/com/example/tugas2/MainActivity.kt
@@ -15,22 +15,16 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.btreg)
-        button.setOnClickListener {
-            Toast.makeText(this, "Berhasil Register", Toast.LENGTH_SHORT).show()
-        }
-        val punyaakun: TextView = findViewById(R.id.punyaakun)
-
-        punyaakun.setOnClickListener {
-            val intentHome = Intent(this, LoginActivity::class.java)
+        val tugas2dan3 = findViewById<Button>(R.id.button3)
+        tugas2dan3.setOnClickListener {
+            val intentHome = Intent(this, RegisterActivity::class.java)
             startActivity(intentHome)
         }
 
-        val kontak: TextView = findViewById(R.id.kontak)
-
-        kontak.setOnClickListener {
-            Intent(Intent.ACTION_DIAL, Uri.parse("tel:08123456789"))
-            startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:08123456789")))
+        val tugas4 = findViewById<Button>(R.id.button4)
+        tugas4.setOnClickListener {
+            val intentHome = Intent(this, RecycleView::class.java)
+            startActivity(intentHome)
         }
     }
 }
